3655f669fb51e5b36fdceedd0be51be54abcae36
kepka ludovic <shionn@gmail.com>
2024-10-26 09:15:09 +0200

    J'ai pas avancer

--- src/main/java/armybuilder/db/dbo/Keyword.java
+++ src/main/java/armybuilder/db/dbo/Keyword.java
@@ -1,9 +1,33 @@
 package armybuilder.db.dbo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Keyword {
 	Attaque,
 	Base,
 	Deploiement,
 	DeploiementDeTerrain,
 	Tir,
+
+	Heros("Héros"),
+	Infanterie,
+	Sorcier,
+
+	Chaos,
+
+	Skavens,
+	Maitreclan("Maîtreclan"),
+
+	;
+
+	private String displayName;
+
+	private Keyword() {
+		this(null);
+		displayName = name();
+	}
+
 }
